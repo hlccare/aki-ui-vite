902743672b623f37fbae2014c54d75267a7e63f1
hlccare <280108904@qq.com>
2023-02-20 10:01:37 +0800

    chore: 添加vite/vue支持

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+
+import SButton from "./button";
+import SFCButton from "./SFCButton.vue";
+
+// createApp(SButton).mount("#app");
+createApp(SFCButton).mount("#app");
