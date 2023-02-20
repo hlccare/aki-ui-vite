aaf90838abbefce4bb98abc7861e4965fc3a46fb
hlccare <280108904@qq.com>
2023-02-20 15:25:17 +0800

    chore: 增加jsx支持

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { createApp } from "vue";
 
 import SButton from "./button";
+import JSXButton from "./JSXButton";
 import SFCButton from "./SFCButton.vue";
 
 // createApp(SButton).mount("#app");
-createApp(SFCButton).mount("#app");
+// createApp(SFCButton).mount("#app");
+createApp(JSXButton).mount("#app");
